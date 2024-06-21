c8fadfd2bc606fbebd4d8a740918257b8a217bcd
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2024-06-21 13:23:22 +0100

    update tests for new behaviour

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pathlib
 import re
 import shutil
 import subprocess
@@ -15,16 +16,20 @@ TEST_OUTPUT_FOLDER = "test/output"
 
 @pytest.mark.parametrize("crate_name", [*CRATES])
 def test_created_datacite_files(crate_name):
+    # Arrange
     crate_path = os.path.join(TEST_DATA_FOLDER, crate_name)
     compare_path = os.path.join(TEST_DATA_FOLDER, f"datacite-out-{crate_name}.json")
     with open(compare_path) as expected:
         expected_json = json.load(expected)
-
-    # note - check_output raises CalledProcessError if exit code is non-zero
-    log = subprocess.check_output(
-        f"python deposit.py {crate_path}", shell=True, text=True
-    )
     output_file = "datacite-out.json"
+
+    # Act
+    # note - check_output raises CalledProcessError if exit code is non-zero
+    # --no-upload prevents upload and generates DataCite files only
+    log = subprocess.check_output(
+        f"python deposit.py {crate_path} --no-upload", shell=True, text=True
+    )
+    # preserve data files and log in TEST_OUTPUT_FOLDER
     shutil.copyfile(
         output_file, os.path.join(TEST_OUTPUT_FOLDER, f"datacite-out-{crate_name}.json")
     )
@@ -34,6 +39,8 @@ def test_created_datacite_files(crate_name):
     ) as log_file:
         log_file.write(log)
 
+    # Assert
+    assert "Created datacite-out.json, skipping upload" in log
     assert os.path.exists(output_file)
     with open(output_file) as output:
         output_json = json.load(output)
@@ -42,12 +49,15 @@ def test_created_datacite_files(crate_name):
 
 @pytest.mark.parametrize("crate_name", [*CRATES])
 def test_created_invenio_records(crate_name):
+    # Arrange
     crate_path = os.path.join(TEST_DATA_FOLDER, crate_name)
     compare_path = os.path.join(TEST_DATA_FOLDER, f"datacite-out-{crate_name}.json")
     with open(compare_path) as expected:
         expected_json = json.load(expected)
         expected_metadata = expected_json["metadata"]
+    expected_log_pattern = r"^Successfully created record (?P<id>[0-9]*)$"
 
+    # Act
     # note - check_output raises CalledProcessError if exit code is non-zero
     log = subprocess.check_output(
         f"python deposit.py {crate_path}", shell=True, text=True
@@ -57,12 +67,10 @@ def test_created_invenio_records(crate_name):
         "w",
     ) as log_file:
         log_file.write(log)
-    match = re.search(
-        r"^Successfully created record (?P<id>[0-9]*)$", log, flags=re.MULTILINE
-    )
+    match = re.search(expected_log_pattern, log, flags=re.MULTILINE)
     record_id = match.group("id")
 
-    # get the record
+    # get the created record
     api_url = credentials.repository_base_url
     api_key = credentials.api_key
     headers = {
@@ -76,6 +84,7 @@ def test_created_invenio_records(crate_name):
     ).json()
     metadata = record["metadata"]
 
+    # Assert
     assert metadata["title"] == expected_metadata["title"]
     assert metadata["upload_type"] == expected_metadata["resource_type"]["id"]
     assert metadata["access_right"] == "open"
@@ -89,9 +98,12 @@ def test_created_invenio_records(crate_name):
         assert len(record["files"]) != 0
         for file in record["files"]:
             filename = file["filename"]
-            with open(
-                os.path.join(TEST_DATA_FOLDER, crate_name, filename)
-            ) as local_file:
+            local_path = next(
+                pathlib.Path(os.path.join(TEST_DATA_FOLDER, crate_name)).glob(
+                    f"**/{filename}"
+                )
+            )
+            with open(local_path) as local_file:
                 remote_file_data = requests.get(
                     file["links"]["download"], headers=headers
                 ).content
