3ec76a996a8092322364fb7f7d4a2e2b4507672e
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2024-07-30 14:35:27 +0100

    more flake8

--- src/rocrate_inveniordm/mapping/converter.py
+++ src/rocrate_inveniordm/mapping/converter.py
@@ -15,8 +15,6 @@ from rocrate_inveniordm.mapping.mapping_utils import (
 from rocrate_inveniordm.mapping.crate_utils import (
     dereference,
     rc_get_rde,
-    get_referenced_entity,
-    get_referenced_entity_from_root,
     get_value_from_rc,
 )
 
