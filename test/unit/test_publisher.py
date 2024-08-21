1f8f797db96a90197e0820bfbfe8bd11f69eb27b
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2024-08-21 11:10:30 +0100

    simplify tests with fixtures

--- test/unit/test_publisher.py
+++ test/unit/test_publisher.py
@@ -22,16 +22,26 @@ publisher_entity = {
 }
 
 
-@pytest.mark.xfail(
-    reason="known bug - https://github.com/ResearchObject/ro-crate-inveniordm/issues/1"
-)
-def test_publisher_string():
-    rc = load_template_rc()
+@pytest.fixture(scope="module", autouse=True)
+def rc():
+    """Creates template RO-Crate metadata."""
+    return load_template_rc()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def dc():
+    """Creates template DataCite metadata."""
+    return setup_dc()
+
+
+# @pytest.mark.xfail(
+#     reason="known bug - https://github.com/ResearchObject/ro-crate-inveniordm/issues/1"
+# )
+def test_publisher_string(rc, dc):
     rc, _ = set_field_in_template_rde("publisher", publisher_string, rc)
     rule_name = "publisher_mapping_1"
     rule = get_single_mapping("publisher", rule_name)
     paths = get_mapping_paths(rc, {rule_name: rule})
-    dc = setup_dc()
 
     dc, _ = apply_mapping(rule, paths, rc, dc)
 
@@ -39,16 +49,13 @@ def test_publisher_string():
     assert dc["metadata"]["publisher"] == publisher_string
 
 
-def test_publisher_entity():
-    rc = load_template_rc()
+def test_publisher_entity(rc, dc):
     rc = add_entity_to_template(publisher_entity, rc)
     rc, _ = set_field_in_template_rde("publisher", {"@id": publisher_entity["@id"]}, rc)
-    dc = setup_dc()
     rule_name = "publisher_mapping_1"
     rule = get_single_mapping("publisher", rule_name)
     paths = get_mapping_paths(rc, {rule_name: rule})
 
     dc, _ = apply_mapping(rule, paths, rc, dc)
 
-    print(dc)
     assert dc["metadata"]["publisher"] == publisher_entity["name"]
